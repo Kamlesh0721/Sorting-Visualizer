3c9e1368adaaba67e4fa69c381b8f6a76d4129a8
Kamlesh Singh <kamleshsingh0198@gmail.com>
2024-08-21 00:56:28 +0530

    add bars in panel

--- src/Structure/SortingPanel.java
+++ src/Structure/SortingPanel.java
@@ -1,6 +1,42 @@
 package Structure;
 
-import javax.swing.*;
+import Utils.Constraints;
 
-public class SortingPanel extends JPanel {
+import javax.swing.*;
+import java.awt.*;
+import java.util.Random;
+
+public class SortingPanel extends JPanel implements Constraints {
+
+    private Random random;
+    private int[] array;
+
+    // Buttons
+    private JButton start;
+    private JButton reset;
+    private JButton insertion;
+
+    public SortingPanel() {
+        random = new Random();
+        array = new int[SIZE];
+        this.setArray();
+    }
+
+    private void setArray() {
+        for (int i = 0; i < this.array.length; i++) {
+            int bar_height = FRAME_HEIGHT - HEADER_SIZE;
+            this.array[i] = random.nextInt(bar_height);
+        }
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        this.setBackground(Color.BLACK);
+        int bar_width = FRAME_WIDTH / SIZE;
+        for (int i = 0; i < array.length; i++) {
+            g.setColor(Color.yellow);
+            g.drawRect(i * bar_width, FRAME_HEIGHT - array[i], bar_width, array[i]);
+        }
+    }
 }
